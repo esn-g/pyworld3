f94a0df62e621f4e97e77a42898388b80947c932
esn-g <152874596+esn-g@users.noreply.github.com>
2024-02-07 13:47:30 +0100

    Sync 1

--- start_resource.py
+++ start_resource.py
@@ -21,13 +21,30 @@
     >>> rsc.init_exogenous_inputs()
     >>> rsc.run_resource()
 '''
-
+import matplotlib.pyplot as plt
 from pyworld3 import Resource
 
+from pyworld3.utils import plot_world_variables
+
 rsc = Resource()
-rsc.set_resource_table_functions()
+rsc.set_resource_table_functions('./my_modified_functions_table_world3.json')
+rsc.set_resource_control()
 rsc.init_resource_variables()
 rsc.init_resource_constants()
 rsc.set_resource_delay_functions()
 rsc.init_exogenous_inputs()
 rsc.run_resource()
+
+plot_world_variables(
+    rsc.time,
+    [rsc.nrfr, rsc.iopc,  rsc.pop, rsc.nrur ],
+    ["NRFR", "IOPC",  "POP", 'NRUR'],
+    [[0, 1], [0, 1e3], [0, 16e9], [0, 1.5*max(rsc.nrur)], [0, 32]],
+    img_background="./img/fig7-7.png",
+    figsize=(7, 5),
+    title="rsc run",
+)
+
+plt.show()
+
+#print(rsc.nrfr)
