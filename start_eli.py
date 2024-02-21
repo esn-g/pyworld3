9fdcce5ccab29198d589e9ddf6e27dc26d940b2c
ElliotTapperKarlsson <elliottk@kth.se>
2024-02-21 16:53:48 +0100

    skit

--- start_eli.py
+++ start_eli.py
@@ -29,7 +29,11 @@ state_variables=np.array([world3.al,
     world3.p4,
     world3.nr])
 
-var_str_list=["al",
+
+
+X_state_matrix=state_variables.T
+
+var_str_list=np.array(["al",
     "pal",
     "uil",
     "lfert",
@@ -40,7 +44,26 @@ var_str_list=["al",
     "p2",
     "p3",
     "p4",
-    "nr"]
+    "nr"]).T       #Transposes it to get each variable as its own column - X matrix but names
+print(var_str_list.shape)
+
+var_str_matrix=np.full((world3.n,12),"a")
+print(var_str_matrix.shape)
+
+print(var_str_matrix)
+
+for var_index, variable_name in enumerate(var_str_list):
+    print(var_index, variable_name)
+    print(var_str_matrix[var_index,:])
+    var_str_matrix=np.char.add(var_str_matrix[var_index,:],variable_name) #Add variable name to entire row
+
+for k_index in range(world3.n):
+    var_str_matrix=np.char.add(var_str_matrix[:,k_index],f"_{k_index}") #Add nr to entire column
+
+
+
+    
+
 
 
 state_array=state_variables
@@ -49,11 +72,13 @@ state_array_k=state_variables[:, 1: ]
 
 state_array_prev=state_variables[:, : (world3.n-1)]
 
+X_state_matrix_0=X_state_matrix[:(world3.n-1),:]  #Removes last row corresponding to kmax
+
 
 def calculate_theta_row(var_index=0, var_array=np.array([]) ):
     var_array_k=var_array[ 1: ,np.newaxis]
     var_array_prev=var_array[ : (world3.n-1) ]
-    print(f"\n_____{var_str_list[var_index]}:_____\n")
+    print(f"\n_____{var_str_list[var_index]}:_____\n") 
     print(state_array_prev.shape)
     print(var_array_k.shape)
     # print(var_array_k)
@@ -62,9 +87,11 @@ def calculate_theta_row(var_index=0, var_array=np.array([]) ):
     # theta=theta.reshape(-1)
     # print(theta.shape)
 
-def construct_A_matrix( array_of_states=np.array([]) ):
+def construct_A_matrix( array_of_vars=np.array([]) ):
     
-    for var_index, var_array in enumerate(array_of_states):
+    for var_index, var_array in enumerate(array_of_vars): #Goes through row by row, where each row is 
+
+        print(var_array)
         calculate_theta_row(var_index, var_array )
 
 
