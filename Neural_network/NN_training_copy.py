0f5cf4df6443e72e0946b3dbeafb56491835fdec
ElliotTapperKarlsson <elliottk@kth.se>
2024-04-08 19:39:51 +0200

    Update NN_training_copy.py

--- Neural_network/NN_training_copy.py
+++ Neural_network/NN_training_copy.py
@@ -1,4 +1,5 @@
 import os
+from re import L
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
@@ -24,11 +25,6 @@ device = (
     else "cpu"
 )
 print(f"Using {device} device")
-
-model=Neural_Network()
-
-
-dataset = CustomDataset("create_dataset/dataset_storage/dataset_runs_1_variance_0_normalized_.json") # Create an instance of your map-style dataset
 
 
 
@@ -61,7 +57,7 @@ class NN_training_run():
         #model,   # MAYBE DO THIS WITHIN THE INIT AND SIMPLY PASS THE SHAPE    
     def __init__(self, 
                 dataset, 
-                num_epochs=100, 
+                num_epochs=1000, 
                 criterion='sum', 
                 learning_rate=1e-4,
                 batch_size=100,                 
@@ -85,6 +81,15 @@ class NN_training_run():
         self.num_epochs=num_epochs
         self.batch_size=batch_size
         self.train_loader=train_loader
+        
+
+        self.hyperparameters_dict={
+                "criterion": criterion ,
+                "learning_rate": learning_rate,
+                "batch_size": batch_size,
+                "NN_depth": NN_depth,
+                "NN_width": NN_width,
+                }
 
         #Values for the NN model
         self.NN_depth=NN_depth
@@ -98,6 +103,9 @@ class NN_training_run():
 
         self.criterion=nn.MSELoss(reduction=criterion)    #Saves lossfunc
         self.optimizer=torch.optim.Adam(self.model.parameters(), lr=learning_rate)
+
+        self.epoch_loss_evolution=torch.empty((self.num_epochs))
+
 
         
 
@@ -141,7 +149,7 @@ class NN_training_run():
         return running_loss #Return the current loss to be saved for each epoch
 
 
-    def train_model(self): #, model, train_loader, criterion, optimizer, num_epochs=100, save=True, savename=None): #Not needed in class
+    def train_model(self, min_delta_error=1e-4): #, model, train_loader, criterion, optimizer, num_epochs=100, save=True, savename=None): #Not needed in class
 
         """
         Train the neural network model epoch for epoch
@@ -165,43 +173,55 @@ class NN_training_run():
         #print(nr_of_batches)
         #batch_loss_evolution=torch.empty((nr_of_batches))
         
-        epoch_loss_evolution=torch.empty((self.num_epochs))
+        #epoch_loss_evolution=torch.empty((self.num_epochs))    #NOW DOING THIS AS A CLASS-ATTRIBUTE
 
 
 
 
         for epoch in range(self.num_epochs): #Run through epochs, go through the data and train, then check loss, run again
-            print(f"Epoch {epoch+1}\n-------------------------------")
+            #print(f"Epoch {epoch+1}\n-------------------------------")
             running_loss=self.single_epoch_training_loop()  #model, train_loader, criterion, optimizer) #Not needed for class
             #test_loop(test_dataloader, model, loss_fn)     # TO BE ADDED LATER
         
             epoch_loss = running_loss / len(self.train_loader.dataset)
-            epoch_loss_evolution[epoch]=epoch_loss  #Add the loss of the current epoch to the tensor of losses
+            self.epoch_loss_evolution[epoch]=epoch_loss  #Add the loss of the current epoch to the tensor of losses
             #print("len train_loader.dataset : ", len(train_loader.dataset))
             #print("Loss.item()=",loss.item())
             #print("size: ",inputs.size(0))
 
-            if (epoch_loss_evolution[epoch-1]-epoch_loss)<1e-4:     #Check if we are still improving
+            if abs(self.epoch_loss_evolution[epoch-1]-epoch_loss)<min_delta_error:     #Check if we are still improving
                 print("Ended at epoch ", epoch, " because learning stagnated" )
                 return  #Exiting the epoch loop would be better, dont have internet right now to check the syntax
 
-            if (epoch+1) % 25 == 0:
+            if (epoch+1) % 50 == 0:
                 print(f"Epoch {epoch+1}/{self.num_epochs}, Loss: {epoch_loss:.4f}\n-------------------------------")
 
-
+        '''
         if save==True:
             if savename==None:
-                savename="model_b["+ str(self.batch_size) + "]_lr["+ str(self.learning_rate) + "]_ep[" + str(num_epochs) + "].pt"
+                savename="model_b["+ str(self.batch_size) + "]_lr["+ str(self.learning_rate) + "]_ep[" + str(self.num_epochs) + "].pt"
             torch.save(self.model, "Neural_network/model/"+savename)
         print("\nDone!\n")
-        return epoch_loss_evolution #, batch_loss_evolution
+        '''
+        print("\nDone!\n")
+        return self.epoch_loss_evolution #, batch_loss_evolution
+        
+    #def return_hyperparams(self):
+
+    def save_model(self, savename=None):
+        if savename==None:
+            savename="model_b["+ str(self.batch_size) + "]_lr["+ str(self.learning_rate) + "]_ep[" + str(self.num_epochs) + "].pt"
+        torch.save(self.model, "Neural_network/model/"+savename)
+    
+    def __str__(self):
+        return "Model with hyperparams: " +  str(self.hyperparameters_dict) + "\n\nGives loss: " +  str(self.epoch_loss_evolution[-1]) 
         
 
 
 
 ####################################################################################################
     
-class NN_training_optimization():
+class NN_hyperparameters_optimization():
     
     #Class for training the NN network, 
     # either for to train a model to be used 
@@ -237,11 +257,20 @@ class NN_training_optimization():
             #Generate_dataset.fetch_dataset(fetch_hyperparams)  #does the same as below
 
             #Fetch hyperparamters stored in json file fetch_hyperparams
-            with open(fetch_hyperparams, "r") as json_file:
+            with open(fetch_hyperparams_path, "r") as json_file:
                 # returns JSON object as a dictionary
                 hyperparams_dict = json.loads(json_file.read())
             
         self.hyperparams_dict=hyperparams_dict
+        self.current_training=NN_training_run()
+        self.hyperparams_stepsize={
+            #"criterion":"sum",
+            "learning_rate": 5e-6, # [1,1e-6, 5e-6],
+            "batch_size":  10, #[1,600, 10],
+            "NN_depth":  1, #[2,10, 1],
+            "NN_width":  2, #[10,30, 2],
+            #"activation_function" : nn.ReLU()
+            }
 
     
         
@@ -262,24 +291,92 @@ class NN_training_optimization():
             current_training_run=NN_training_run(**self.hyperparams_dict)
         else:
             current_training_run=NN_training_run(**dict_of_parameters)
-        return current_training_run
+        self.current_training=current_training_run            #return current_training_run
+
+
+
+    def evaluate_hyperparameters(self, dict_of_parameters=None):
+
+        if dict_of_parameters==None:
+            dict_of_parameters=self.hyperparams_dict
+
+
+    def hyperparameter_space(self):
+
+        Learning_rate_stepsize=5e-6
+        batch_size_stepsize=5
+        NN_depth_stepsize=1
+        NN_width_stepsize=1
+
+        Learning_rate_num_steps=2
+        batch_size_num_steps=2
+        NN_depth_num_steps=1
+        NN_width_num_steps=1
+        
+
+        Learning_rate_steps=np.linspace(self.hyperparams_dict["learning_rate"])
+        batch_size_steps=np.linspace(self.hyperparams_dict["batch_size"])
+        NN_depth_steps=np.linspace(self.hyperparams_dict["NN_depth"])
+        NN_width_steps=np.linspace(self.hyperparams_dict["NN_width"])
+
+        for lr_val in np.arange(Learning_rate_steps):
+            pass
+
+        for i in self.hyperparams_stepsize.keys():
+            #self.hyperparams_stepsize(i)[2]
+            pass
+
+        
+        
+        
 
     def sweep_training(self, iters=10, parameter="Epochs"):
 
         current_hyperparams=self.hyperparams_dict
 
-        current_training_run=self.run_training()    #returns our current run
+        #Create a room of variance of hyperparams
+        #Call the sweeping for all these
+        #Find best one, 
+        # Create room again, a lot smaller variance
+        # Find best one
+        #Until certain smallest error diff is reached
 
-        
+        self.run_training()    #Creates a NN_training_run object and saves in self.current_training_run
+
+
 
         #Create a tensor a room of possible hyperparams and their respective loss
         #The best one is saved
 
+
         
         #current_model=Neural_Network(hidden_sizes=model_hidden_shape)
         for i in np.arange(iters):
+            self.run_training()
 
-            train_model(model=model, train_loader=train_loader, criterion=criterion, optimizer=optimizer, num_epochs=epochs)
+
+
+            #train_model(model=model, train_loader=train_loader, criterion=criterion, optimizer=optimizer, num_epochs=epochs)
+
+
+
+
+#model=Neural_Network()
+
+
+dataset = CustomDataset("create_dataset/dataset_storage/dataset_runs_1_variance_0_normalized_.json") # Create an instance of your map-style dataset
+
+
+training_obj=NN_training_run(dataset)
+training_obj.train_model()
+print(training_obj)
+
+
+
+
+
+
+
 
 ####################################################################################################
 
